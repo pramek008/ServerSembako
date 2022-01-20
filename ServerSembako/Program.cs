e7698567cf4abe7cb865ba08e353918d7fcd8eb3
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2022-01-20 19:22:43 +0700

    Add project files.

---
+++ ServerSembako/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using ServiceSembako;
+
+namespace ServerSembako
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(Service1));
+                hostObjek.Open();
+                Console.WriteLine("Server siap");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch(Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
